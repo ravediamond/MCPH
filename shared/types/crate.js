5adb4c263773c65cc6d52038c5970a29a29deb40
ravindu.somawansa <ravindu.somawansa@gmail.com>
2025-06-15 21:49:52 +0200

    feat: working mcp server

--- shared/types/crate.js
+++
@@ -1,8 +0,0 @@
-export var CrateCategory;
-(function (CrateCategory) {
-    CrateCategory["MARKDOWN"] = "markdown";
-    CrateCategory["CODE"] = "code";
-    CrateCategory["IMAGE"] = "image";
-    CrateCategory["JSON"] = "json";
-    CrateCategory["BINARY"] = "binary";
-})(CrateCategory || (CrateCategory = {}));
